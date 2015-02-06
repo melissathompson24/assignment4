ac3cac07ad3f607c28dd4561141ca8ad628c0f89
melissathompson24 <melissa.thompson24@gmail.com>
2015-02-05 22:23:34 -0700

    Finished Assignment 4

--- jQuery/form.js
+++ jQuery/form.js
@@ -6,58 +6,15 @@ $(document).ready(function() {
 	$(":text").after("<span> </span>");
 
 	//track how many digits are entered into phone entry
-	  			/*$("#phone").keyup(function(){
-	  				var phoneLength = $('#phone').length;
-	  				$("#phoneLength").append(phoneLength + "Digits entered");
-	  				if (phoneLength < 10) {
-	  					$("#phone").addClass('inValid');
-	  				} else if (phoneLength ===10) {
-	  					$("#phone").addClass('Valid');
-	  				}
-				});*/
+		$('#phone').keyup(function () {
+		    if ($('#phone').val().length < 10) {
+		        $(this).addClass('inValid');
+		    } else {
+		        $(this).removeClass('inValid');
+		        $(this).addClass('Valid');
+		    }
+		});
 
-
-			
-			$("#phone").keyup(function(){
-				var phoneLength = $("#phone").length;
-				console.log(phoneLength);
-  				if (phoneLength > 9) {
-  					$("#phone").removeClass('inValid');
-  					$("#phone").addClass('Valid');
-  				}
-  				
-  			});
-
-
-
-/*$("#phone").keyup(function(){
-				var phoneLength = $(this).length;
-				console.log(phoneLength);
-  				if (1 > phoneLength < 10) {
-  					$("#phone").addClass('inValid');
-
-				} else if (phoneLength > 9) {
-							$("#phone").removeClass('inValid');
-							$("#phone").addClass('Valid');
-  				}
-  				
-  			});*/
-
-
-
-	  			
-  			
-
-
-			 	/*var maxLength = 10;
-			 	$('#phone').keyup(function() {
-			 		var length = $(this).val().length;
-			 		var length = maxLength - length;
-			 		$('#phoneLength').text(length);
-			 	});*/
-			 	/*$("#phone").keyup(function() {
-			 	alert("keyup event occured!");
-			 	});*/
 
 	//once form is submitted
 	 	$("#form").submit(
@@ -88,6 +45,10 @@ $(document).ready(function() {
 
   				//make sure phone is in correct format
 
+  				var phoneValue = $('#phone').val();
+  				console.log(phoneValue);
+  				var phoneFormatted = "(" + phoneValue.substr(0,3) + ") " + phoneValue.substr(3,3) + "-" + phoneValue.substr(6,4);
+  				$('#phone').val(phoneFormatted);
 
 
 	  			//validate email address
@@ -108,7 +69,8 @@ $(document).ready(function() {
 	  			//prevent form from being submitted if any entries are invalid
 	  			if (isValid == false) {event.preventDefault(); 
 	  				$('input').addClass('inValid');
-
+	  			} else {
+	  				confirm("Thanks! You've submitted the following information:" + name + ", " + address + ", " + phoneFormatted + ", and " + email + ".");
 	  			}
 				     
 
